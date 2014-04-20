d331b13fffa8ec7368c96abb92b6e07f256380e6
Imran Raja <imranraja85@gmail.com>
2014-04-20 22:08:54 +0000

    added ability to get subscriber signups on launch page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,15 @@ class PagesController < ApplicationController
   def launch
     render :layout => false
   end
+
+  def subscribe
+    Subscriber.create(subscriber_params)
+
+    redirect_to :back
+  end
+
+  private
+  def subscriber_params
+    params.permit(:email)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'pages#launch'
+  post 'subscribe', to: 'pages#subscribe'
 end
