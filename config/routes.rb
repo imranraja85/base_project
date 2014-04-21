05da8725174bc8a49da7d7fc8cc7565499b09284
Imran Raja <imranraja85@gmail.com>
2014-04-21 07:15:09 +0000

    wiring up landing nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root 'pages#launch'
+  #root 'pages#home'
+
   post 'subscribe', to: 'pages#subscribe'
+
+  get 'home',    to: 'pages#home'
+  get 'tour',    to: 'tour#home'
+  get 'start',   to: 'pages#start'
+  get 'contact', to: 'pages#contact'
 end
